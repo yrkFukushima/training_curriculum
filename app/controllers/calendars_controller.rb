a4c3eabfd01574f4f7994cd2afe11b85984feeb6
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-04-27 17:50:56 +0900

    Issue5

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
+    
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -35,11 +35,9 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< Updated upstream
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-=======
+
       days = {month: (@todays_date+x).month, date: (@todays_date+x).day, plans: today_plans}
->>>>>>> Stashed changes
+
       @week_days.push(days)
     end
 
