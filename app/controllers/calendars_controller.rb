ace80452a5e39829a5c369d31399ec50336b78d1
yrkFukushima <82563469+yrkFukushima@users.noreply.github.com>
2021-04-27 17:14:13 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -15,7 +16,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
@@ -34,7 +35,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+<<<<<<< Updated upstream
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+=======
+      days = {month: (@todays_date+x).month, date: (@todays_date+x).day, plans: today_plans}
+>>>>>>> Stashed changes
       @week_days.push(days)
     end
 
